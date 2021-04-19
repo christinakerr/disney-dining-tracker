ffbf000abafe05ba7d5b63249d6427e9bdad287d
Christi Kerr <christinakerr394@gmail.com>
2021-04-19 12:01:56 -0500

    Initialized router and added home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import Container from "/components/Container.js"
-import Header from "/components/Header.js"
+import Container from "./components/Container.js"
+import Header from "./components/Header.js"
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home"
 
 function App() {
   return (
     <div>
       <Header />
-      <Container></Container>
+      <Container>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </BrowserRouter>
+      </Container>
     </div>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,4 @@
-import Header from "../components/Header";
-import Container from "../components/Container"
+
 import Button from "../components/Button";
 import H2 from "../components/Text/H2";
 import CreditsRemaining from "../components/CreditsRemaining";
@@ -12,27 +11,21 @@ function Home(){
     if(!currentVacation){
         return (
             <div>
-            <Header />
-            <Container>
                 <Button>Add Disney World Vacation</Button>
                 <H2>Previous Vacations</H2>
                 <Button>Spring 2020</Button>
                 <Button>Spring 2019</Button>
                 <Button>Spring 2018</Button>
                 <Button>Winter 2017</Button>
-            </Container>
             </div>
         );
     } else {
         return (
             <div>
-            <Header />
-            <Container>
                 <CreditsRemaining />
                 <FoodItem name="Mickey Ice Cream" credit="1 Snack" location="Stall in front of castle" park="Magic Kingdom"/>
                 <FoodItem name="Breakfast Buffet" credit="1 Table-Service" location="Crystal Palace" park="Magic Kingdom"/>
                 <Button>Mark Vacation as Completed</Button>
-            </Container>
             </div>
         );
     }
