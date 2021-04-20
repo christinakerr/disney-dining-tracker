c9645289ade30185be3de8e219c26c2248c62f76
Christi Kerr <christinakerr394@gmail.com>
2021-04-20 10:47:24 -0500

    Built skeleton of Add Vacation page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Container from "./components/Container.js"
 import Header from "./components/Header.js"
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home"
+import AddVacation from "./pages/AddVacation.js";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/add-vacation" component={AddVacation} />
           </Switch>
         </BrowserRouter>
       </Container>
---
+++ client/src/pages/AddVacation.js
@@ -0,0 +1,21 @@
+import Form from "../components/Forms/Form"
+import Button from "../components/Button";
+import H1 from "../components/Text/H1";
+import Input from "../components/Forms/Input";
+
+
+
+function AddVacation(){
+    return(
+        <div>
+            <H1>Add Vacation</H1>
+            <Form>
+                <Input type="text" label="Name" input="vacation-name"/>
+                <Input type="number" label="Nights in a Walt Disney World Resort" input="vacation-days"/>
+                <Button>Add</Button>
+            </Form>
+        </div>
+    )
+}
+
+export default AddVacation;
