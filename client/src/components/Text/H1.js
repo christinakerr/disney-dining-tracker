375b6f0083b4073a91994881d1a714cc15325dc3
Christi Kerr <christinakerr394@gmail.com>
2021-04-21 11:00:03 -0500

    Centered all H1s

--- client/src/components/Text/H1.js
+++ client/src/components/Text/H1.js
@@ -1,9 +1,15 @@
 /** @jsxImportSource @emotion/react */
 
+import { css } from '@emotion/react'
+
 function H1(props){
     return (
-        <h1>{props.children}</h1>
+        <h1 css={h1Style}>{props.children}</h1>
     );
 }
 
 export default H1;
+
+const h1Style = css`
+    text-align: center;
+`
