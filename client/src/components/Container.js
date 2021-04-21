c28aa8fa0b9c22e2a9f25e0a04f1f0f3fc23ffe6
Christi Kerr <christinakerr394@gmail.com>
2021-04-21 10:28:49 -0500

    Added responsive styles to Container component

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,9 +1,36 @@
 /** @jsxImportSource @emotion/react */
 
-function Container(props){
+import { css } from '@emotion/react'
+
+function Container(props) {
     return (
-        <div>{props.children}</div>
+        <div css={containerStyle}>{props.children}</div>
     );
 }
 
 export default Container;
+
+const containerStyle = css`
+    margin: 0 auto;
+    @media (min-width: 320px) {
+        max-width: 310px;
+    }
+    @media (min-width: 425px) {
+        max-width: 400px;
+    }
+    @media (min-width: 576px) {
+        max-width: 540px;
+    }
+    @media (min-width: 768px) {
+        max-width: 720px;
+    }
+    @media (min-width: 992px) {
+        max-width: 960px;
+    }
+    @media (min-width: 1200px) {
+        max-width: 1140px;
+    }
+    @media (min-width: 1400px) {
+        max-width: 1320px;
+    }
+`
