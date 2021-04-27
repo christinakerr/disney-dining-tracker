afc03923e4bcbed79c05e19ae24a5896d94cc418
Christi Kerr <christinakerr394@gmail.com>
2021-04-27 10:39:12 -0500

    Home page generates previous vacation list from temporary user data state

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import { Provider } from 'react-redux'
+
+
 import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+      <App />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,19 +4,27 @@ import H2 from "../components/Text/H2";
 import CreditsRemaining from "../components/CreditsRemaining";
 import FoodItem from "../components/FoodItem";
 
-let currentVacation = true;
+import React, { useState } from "react";
 
-function Home(){
+let currentVacation = false;
 
-    if(!currentVacation){
+function Home() {
+
+    const [tempState] = useState(userData);
+
+    const previous = tempState.filter(vacation => !vacation.current)
+
+    if (!currentVacation) {
         return (
             <div>
                 <Button big heading>Add Disney World Vacation</Button>
                 <H2 center>Previous Vacations</H2>
-                <Button big>Spring 2020</Button>
-                <Button big>Spring 2019</Button>
-                <Button big>Spring 2018</Button>
-                <Button big>Winter 2017</Button>
+
+                {
+                    previous.map(vacation => {
+                        return <Button big>{vacation.name}</Button>
+                    })
+                }
             </div>
         );
     } else {
@@ -24,8 +32,8 @@ function Home(){
             <div>
                 <CreditsRemaining />
                 <Button big>Add Food</Button>
-                <FoodItem name="Mickey Ice Cream" credit="1 Snack" location="Stall in front of castle" park="Magic Kingdom"/>
-                <FoodItem name="Breakfast Buffet" credit="1 Table-Service" location="Crystal Palace" park="Magic Kingdom"/>
+                <FoodItem name="Mickey Ice Cream" credit="1 Snack" location="Stall in front of castle" park="Magic Kingdom" />
+                <FoodItem name="Breakfast Buffet" credit="1 Table-Service" location="Crystal Palace" park="Magic Kingdom" />
                 <Button big>Mark Vacation as Complete</Button>
             </div>
         );
@@ -33,3 +41,112 @@ function Home(){
 }
 
 export default Home;
+
+
+const userData = [
+    {
+        id: 1,
+        name: "Spring 2020",
+        current: true,
+        nights: 5,
+        food: [
+            {
+                id: 1,
+                park: "Magic Kingdom",
+                restaurant: "Crystal Palace",
+                food: "Breakfast Buffet",
+                drink: "Orange Juice",
+                credit: "table-service",
+                date: 20200321,
+            },
+            {
+                id: 2,
+                park: "Magic Kingdom",
+                restaurant: "Pecos Bill's",
+                food: "Rice Bowl",
+                drink: "Lemonade",
+                dessert: "Brownie",
+                credit: "quick-service",
+                date: 20200322,
+            },
+            {
+                id: 3,
+                park: "Magic Kingdom",
+                restaurant: "Gaston's Tavern",
+                food: "Cinnamon Roll",
+                credit: "snack",
+                date: 202003221,
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Spring 2019",
+        current: false,
+        nights: 5,
+        food: [
+            {
+                id: 1,
+                park: "Magic Kingdom",
+                restaurant: "Crystal Palace",
+                food: "Breakfast Buffet",
+                drink: "Orange Juice",
+                credit: "table-service",
+                date: 20200321,
+            },
+            {
+                id: 2,
+                park: "Magic Kingdom",
+                restaurant: "Pecos Bill's",
+                food: "Rice Bowl",
+                drink: "Lemonade",
+                dessert: "Brownie",
+                credit: "quick-service",
+                date: 20200322,
+            },
+            {
+                id: 3,
+                park: "Magic Kingdom",
+                restaurant: "Gaston's Tavern",
+                food: "Cinnamon Roll",
+                credit: "snack",
+                date: 202003221,
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Spring 2018",
+        current: false,
+        nights: 5,
+        food: [
+            {
+                id: 1,
+                park: "Magic Kingdom",
+                restaurant: "Crystal Palace",
+                food: "Breakfast Buffet",
+                drink: "Orange Juice",
+                credit: "table-service",
+                date: 20200321,
+            },
+            {
+                id: 2,
+                park: "Magic Kingdom",
+                restaurant: "Pecos Bill's",
+                food: "Rice Bowl",
+                drink: "Lemonade",
+                dessert: "Brownie",
+                credit: "quick-service",
+                date: 20200322,
+            },
+            {
+                id: 3,
+                park: "Magic Kingdom",
+                restaurant: "Gaston's Tavern",
+                food: "Cinnamon Roll",
+                credit: "snack",
+                date: 202003221,
+            }
+        ]
+    }
+]
