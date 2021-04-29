d529f2c927ca3f4115bd11a3df86616b80a3c704
Christi Kerr <christinakerr394@gmail.com>
2021-04-29 10:04:40 -0500

    Connected CreditsRemaining component to state

--- client/src/components/CreditsRemaining.js
+++ client/src/components/CreditsRemaining.js
@@ -7,7 +7,7 @@ import { useSelector } from 'react-redux'
 
 function CreditsRemaining(){
 
-    const state = useSelector((state) => state)
+    const userData = useSelector((state) => state.userData)
 
     const current = userData.filter(vacation => vacation.current);
 
@@ -61,110 +61,110 @@ const container = css`
     display: flex;
 `
 
-const userData = [
-    {
-        id: 1,
-        name: "Spring 2020",
-        current: true,
-        nights: 5,
-        food: [
-            {
-                id: 1,
-                park: "Magic Kingdom",
-                restaurant: "Crystal Palace",
-                food: "Breakfast Buffet",
-                drink: "Orange Juice",
-                credit: "1 Table-Service",
-                date: 20200321,
-            },
-            {
-                id: 2,
-                park: "Magic Kingdom",
-                restaurant: "Pecos Bill's",
-                food: "Rice Bowl",
-                drink: "Lemonade",
-                dessert: "Brownie",
-                credit: "1 Quick-Service",
-                date: 20200322,
-            },
-            {
-                id: 3,
-                park: "Magic Kingdom",
-                restaurant: "Gaston's Tavern",
-                food: "Cinnamon Roll",
-                credit: "1 Snack",
-                date: 202003221,
-            }
-        ]
-    },
-    {
-        id: 2,
-        name: "Spring 2019",
-        current: false,
-        nights: 5,
-        food: [
-            {
-                id: 1,
-                park: "Magic Kingdom",
-                restaurant: "Crystal Palace",
-                food: "Breakfast Buffet",
-                drink: "Orange Juice",
-                credit: "1 Table-Service",
-                date: 20200321,
-            },
-            {
-                id: 2,
-                park: "Magic Kingdom",
-                restaurant: "Pecos Bill's",
-                food: "Rice Bowl",
-                drink: "Lemonade",
-                dessert: "Brownie",
-                credit: "1 Quick-Service",
-                date: 20200322,
-            },
-            {
-                id: 3,
-                park: "Magic Kingdom",
-                restaurant: "Gaston's Tavern",
-                food: "Cinnamon Roll",
-                credit: "1 Snack",
-                date: 202003221,
-            }
-        ]
-    },
-    {
-        id: 3,
-        name: "Spring 2018",
-        current: false,
-        nights: 5,
-        food: [
-            {
-                id: 1,
-                park: "Magic Kingdom",
-                restaurant: "Crystal Palace",
-                food: "Breakfast Buffet",
-                drink: "Orange Juice",
-                credit: "1 Table-Service",
-                date: 20200321,
-            },
-            {
-                id: 2,
-                park: "Magic Kingdom",
-                restaurant: "Pecos Bill's",
-                food: "Rice Bowl",
-                drink: "Lemonade",
-                dessert: "Brownie",
-                credit: "1 Quick-Service",
-                date: 20200322,
-            },
-            {
-                id: 3,
-                park: "Magic Kingdom",
-                restaurant: "Gaston's Tavern",
-                food: "Cinnamon Roll",
-                credit: "1 Snack",
-                date: 202003221,
-            }
-        ]
-    }
-]
+// const userData = [
+//     {
+//         id: 1,
+//         name: "Spring 2020",
+//         current: true,
+//         nights: 5,
+//         food: [
+//             {
+//                 id: 1,
+//                 park: "Magic Kingdom",
+//                 restaurant: "Crystal Palace",
+//                 food: "Breakfast Buffet",
+//                 drink: "Orange Juice",
+//                 credit: "1 Table-Service",
+//                 date: 20200321,
+//             },
+//             {
+//                 id: 2,
+//                 park: "Magic Kingdom",
+//                 restaurant: "Pecos Bill's",
+//                 food: "Rice Bowl",
+//                 drink: "Lemonade",
+//                 dessert: "Brownie",
+//                 credit: "1 Quick-Service",
+//                 date: 20200322,
+//             },
+//             {
+//                 id: 3,
+//                 park: "Magic Kingdom",
+//                 restaurant: "Gaston's Tavern",
+//                 food: "Cinnamon Roll",
+//                 credit: "1 Snack",
+//                 date: 202003221,
+//             }
+//         ]
+//     },
+//     {
+//         id: 2,
+//         name: "Spring 2019",
+//         current: false,
+//         nights: 5,
+//         food: [
+//             {
+//                 id: 1,
+//                 park: "Magic Kingdom",
+//                 restaurant: "Crystal Palace",
+//                 food: "Breakfast Buffet",
+//                 drink: "Orange Juice",
+//                 credit: "1 Table-Service",
+//                 date: 20200321,
+//             },
+//             {
+//                 id: 2,
+//                 park: "Magic Kingdom",
+//                 restaurant: "Pecos Bill's",
+//                 food: "Rice Bowl",
+//                 drink: "Lemonade",
+//                 dessert: "Brownie",
+//                 credit: "1 Quick-Service",
+//                 date: 20200322,
+//             },
+//             {
+//                 id: 3,
+//                 park: "Magic Kingdom",
+//                 restaurant: "Gaston's Tavern",
+//                 food: "Cinnamon Roll",
+//                 credit: "1 Snack",
+//                 date: 202003221,
+//             }
+//         ]
+//     },
+//     {
+//         id: 3,
+//         name: "Spring 2018",
+//         current: false,
+//         nights: 5,
+//         food: [
+//             {
+//                 id: 1,
+//                 park: "Magic Kingdom",
+//                 restaurant: "Crystal Palace",
+//                 food: "Breakfast Buffet",
+//                 drink: "Orange Juice",
+//                 credit: "1 Table-Service",
+//                 date: 20200321,
+//             },
+//             {
+//                 id: 2,
+//                 park: "Magic Kingdom",
+//                 restaurant: "Pecos Bill's",
+//                 food: "Rice Bowl",
+//                 drink: "Lemonade",
+//                 dessert: "Brownie",
+//                 credit: "1 Quick-Service",
+//                 date: 20200322,
+//             },
+//             {
+//                 id: 3,
+//                 park: "Magic Kingdom",
+//                 restaurant: "Gaston's Tavern",
+//                 food: "Cinnamon Roll",
+//                 credit: "1 Snack",
+//                 date: 202003221,
+//             }
+//         ]
+//     }
+// ]
