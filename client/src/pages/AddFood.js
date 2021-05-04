c095a572aa63d89443cefb1337eee3edadf8c027
Christi Kerr <christinakerr394@gmail.com>
2021-05-04 10:28:17 -0500

    Built food object to dispatch to reducer when user adds a food

--- client/src/pages/AddFood.js
+++ client/src/pages/AddFood.js
@@ -34,19 +34,35 @@ function AddFood() {
 
     function handleSubmit(event){
         event.preventDefault(event);
+
+        const credit = [event.target[7], event.target[8], event.target[9], event.target[10], event.target[11]].filter(selection => selection.checked)[0].value;
+
         const park = event.target[0].value;
         const restaurant = event.target[1].value
         const food = event.target[2].value;
         const drink = event.target[5].value;
         const dessert = event.target[6].value;
-        const credit = event.target[7].value;
-        const photo = event.target[12].value;
-        const date = event.target[13].value;
+        // const photo = event.target[12].value;
+        const date = event.target[13].value.replace(/(-)/g, "");
         const note = event.target[14].value;
-        console.log(park, restaurant, food, drink, dessert, credit);
-        console.log(photo);
-        console.log(date);
-        console.log(note);
+
+        const foodObj = {
+            "park": park,
+            "restaurant": restaurant,
+            "food": food,
+            "drink": drink,
+            "dessert": dessert,
+            "credit": credit,
+            "date": date,
+            "note": note
+        }
+
+        for (let prop in foodObj){
+            if (foodObj.hasOwnProperty(prop) && foodObj[prop] === ""){
+                delete foodObj[prop];
+            }
+        }
+        console.log(foodObj)
     }
 
 
@@ -74,11 +90,11 @@ function AddFood() {
 
                     <P label>Dining Credit</P>
 
-                    <Input type="radio" id="snack" label="Snack" input="credit" value="snack"  />
-                    <Input type="radio" id="quick-service" label="Quick-Service" input="credit" value="quick-service" />
-                    <Input type="radio" id="table-service" label="Table-Service" input="credit" value="table-service" />
-                    <Input type="radio" id="two-credit-table-service" label="2 Credit Table-Service" input="credit" value="two-credit-table-service" />
-                    <Input type="radio" id="no-plan" label="Not on Dining Plan" input="credit" value="no-plan" />
+                    <Input type="radio" id="snack" label="Snack" input="credit" value="1 Snack"  />
+                    <Input type="radio" id="quick-service" label="Quick-Service" input="credit" value="1 Quick-Service" />
+                    <Input type="radio" id="table-service" label="Table-Service" input="credit" value="1 Table-Service" />
+                    <Input type="radio" id="two-credit-table-service" label="2 Credit Table-Service" input="credit" value="2 Table-Service" />
+                    <Input type="radio" id="no-plan" label="Not on Dining Plan" input="credit" value="Not on Plan" />
 
                     <Input type="file" label="Upload Photo" input="photo" />
 
