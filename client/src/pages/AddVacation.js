e3de344afafd8c11d31a01aa8fc91fbbe416fb83
Christi Kerr <christinakerr394@gmail.com>
2021-05-07 10:45:57 -0500

    Switched add vacation form to controlled component

--- client/src/pages/AddVacation.js
+++ client/src/pages/AddVacation.js
@@ -8,12 +8,20 @@ import Header from "../components/Header.js"
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom'
-
+import { useState } from 'react';
 
 function AddVacation() {
 
     const dispatch = useDispatch();
     const history = useHistory();
+
+    const [nameInput, setNameInput] = useState()
+    const [nightsInput, setNightsInput] = useState()
+
+    let disabled = false;
+    if (!nameInput || !nightsInput){
+        disabled = true;
+    }
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -38,9 +46,10 @@ function AddVacation() {
             <Container>
                 <H1>Add Vacation</H1>
                 <Form id="addVacation" onSubmit={handleSubmit}>
-                    <Input type="text" label="Vacation Name" input="vacation-name" />
-                    <Input type="number" pattern="[0-9]*" label="Nights in a Walt Disney World Resort" input="vacation-days" />
-                    <Button big type="submit" form="addVacation" value="Add Vacation">Add</Button>
+                    <Input type="text" label="Vacation Name *" input="vacation-name" stateProp={nameInput} onChange={(e) => setNameInput(e.target.value)} />
+                    <Input type="number" pattern="[0-9]*" label="Nights in a Walt Disney World Resort Hotel *" input="vacation-days" stateProp={nightsInput} onChange={(e) => setNightsInput(e.target.value)} />
+                    <br />
+                    <Button big type="submit" form="addVacation" value="Add Vacation" disabled={disabled}>Add Vacation</Button>
                 </Form>
             </Container>
         </div>
