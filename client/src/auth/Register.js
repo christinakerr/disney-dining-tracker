c9b8d6f36e9d2b654994ee243d812007abbf3207
Christi Kerr <christinakerr394@gmail.com>
2021-05-13 11:37:25 -0500

    Built structure of Register page

---
+++ client/src/auth/Register.js
@@ -0,0 +1,63 @@
+import Form from "../components/Forms/Form"
+import Button from "../components/Button";
+import H1 from "../components/Text/H1";
+import Input from "../components/Forms/Input";
+import Container from "../components/Container.js"
+import Header from "../components/Header.js"
+import P from "../components/Text/P"
+
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { useHistory, Link } from 'react-router-dom'
+import { useState } from 'react';
+
+function Register() {
+
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const [nameInput, setNameInput] = useState()
+    const [emailInput, setEmailInput] = useState()
+    const [passwordInput, setPasswordInput] = useState()
+    const [password2Input, setPassword2Input] = useState()
+    const [errors, setErrors] = useState({})
+
+    let disabled = false;
+    // if (!nameInput || !nightsInput){
+    //     disabled = true;
+    // }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const vacationObj = {
+            // "name": nameInput,
+            // "current": true,
+            // "nights": nightsInput,
+            // "food": [],
+            // "id": uuidv4()
+        }
+        dispatch({type: "ADDVACATION", payload: vacationObj});
+        history.push("/")
+    }
+
+    return (
+        <div>
+            <Header />
+            <Container>
+                <H1>Register</H1>
+                <Form noValidate id="register" onSubmit={handleSubmit}>
+                    <Input type="text" label="Name" input="name" stateProp={nameInput} onChange={(e) => setNameInput(e.target.value)} error={errors.name} />
+                    <Input type="email" label="Email" input="email" stateProp={emailInput} onChange={(e) => setEmailInput(e.target.value)} error={errors.email} />
+                    <Input type="password" label="Password" input="password" stateProp={passwordInput} onChange={(e) => setPasswordInput(e.target.value)} error={errors.password} />
+                    <Input type="password" label="Confirm Password" input="password2" stateProp={password2Input} onChange={(e) => setPassword2Input(e.target.value)} error={errors.password2} />
+                    <br />
+                    <Button big type="submit" form="register" value="register" disabled={disabled}>Register</Button>
+                </Form>
+                <P>Already have an account? <Link to="/login">Log in</Link></P>
+            </Container>
+        </div>
+    )
+}
+
+export default Register;
--- client/src/components/Forms/Form.js
+++ client/src/components/Forms/Form.js
@@ -1,6 +1,6 @@
 function Form(props){
     return (
-        <form id={props.id} onSubmit={props.onSubmit}>{props.children}</form>
+        <form id={props.id} onSubmit={props.onSubmit}>{props.children} noValidate={props.noValidate}</form>
     );
 }
 
