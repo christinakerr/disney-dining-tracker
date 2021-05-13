cf828525db1543c1c685112e243fa8daae8aaed7
Christi Kerr <christinakerr394@gmail.com>
2021-05-13 11:59:58 -0500

    Built error reducer

---
+++ client/src/reducers/errorReducers.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+export default function errorReducers(state = initialState, action) {
+    switch (action.type){
+        case "GET_ERRORS":
+            return action.payload;
+        default:
+            return state;
+    }
+}
